74870ad0f5aad92ec926f4e8a76cf545f9bd5e4b
Masanori Ebizuka <a2005me@aiit.ac.jp>
2021-08-07 19:37:37 +0900

    test作成

--- report5-test.sh
+++ report5-test.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
 tmp=/tmp/$$ # 変数を使って表記を短く
-#echo "input 2 argments" > $tmp-args # 回答準備：引数の数
-#echo "input natural number" > $tmp-nat # 回答準備：数字じゃない
+
 
 ERROR_EXIT () {
-    echo "$1" >&2 # エラーメッセージ(引数1)を標準エラー出⼒に表⽰
+    #echo "$1" >&2 # エラーメッセージ(引数1)を標準エラー出⼒に表⽰
+    cat $tmp-error
     rm -f $tmp-* # 作ったファイルの削除
     exit 1 # エラー終了
 }
+
 # テスト開始
 echo "teat1-1: 正常系"
 ./report5.sh 5 10 > $tmp-result
 echo "5" > $tmp-ans
-diff $tmp-result $tmp-ans || ERROR_EXIT "error in test1-1"
-rm -f $tmp-*
+diff $tmp-result $tmp-ans || echo "error in test1-1" >> $tmp-error
 
 echo "teat1-2: 正常系"
 ./report5.sh 21 14 > $tmp-result
 echo "7" > $tmp-ans
-diff $tmp-result $tmp-ans || ERROR_EXIT "error in test1-2"
-rm -f $tmp-*
+diff $tmp-result $tmp-ans || echo "error in test1-2" >> $tmp-error
 
 echo "teat2-1: 異常系"
 ./report5.sh > $tmp-result
 echo "Error!" > $tmp-ans
-diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-1"
-rm -f $tmp-*
+diff $tmp-result $tmp-ans || echo "error in test2-1" >> $tmp-error
 
 echo "teat2-2: 異常系"
 ./report5.sh 21 > $tmp-result
 echo "Error!k" > $tmp-ans
-diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-2"
-rm -f $tmp-*
+diff $tmp-result $tmp-ans || echo "error in test2-2" >> $tmp-error
 
 echo "teat2-3: 異常系"
 ./report5.sh -21 > $tmp-result
 echo "Error!" > $tmp-ans
-diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-3"
-rm -f $tmp-*
+diff $tmp-result $tmp-ans || echo "error in test2-3" >> $tmp-error
 
 echo "teat2-4: 異常系"
 ./report5.sh 21 -1 > $tmp-result
 echo "Error!k" > $tmp-ans
-diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-4"
-rm -f $tmp-*
+diff $tmp-result $tmp-ans || echo "error in test2-4" >> $tmp-error
+
+if [ -f $ $tmp-error ]; then
+    ERROR_EXIT
+}
