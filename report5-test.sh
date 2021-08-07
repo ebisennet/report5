01f168ccc7815838dc8fabb21cbc8de257c8712c
Masanori Ebizuka <a2005me@aiit.ac.jp>
2021-08-07 19:28:50 +0900

    test作成

--- report5-test.sh
+++ report5-test.sh
@@ -14,28 +14,34 @@ echo "teat1-1: 正常系"
 ./report5.sh 5 10 > $tmp-result
 echo "5" > $tmp-ans
 diff $tmp-result $tmp-ans || ERROR_EXIT "error in test1-1"
+rm -f $tmp-*
 
 echo "teat1-2: 正常系"
 ./report5.sh 21 14 > $tmp-result
 echo "7" > $tmp-ans
 diff $tmp-result $tmp-ans || ERROR_EXIT "error in test1-2"
+rm -f $tmp-*
 
 echo "teat2-1: 異常系"
 ./report5.sh > $tmp-result
 echo "Error!" > $tmp-ans
 diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-1"
+rm -f $tmp-*
 
 echo "teat2-2: 異常系"
 ./report5.sh 21 > $tmp-result
 echo "Error!k" > $tmp-ans
 diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-2"
+rm -f $tmp-*
 
 echo "teat2-3: 異常系"
 ./report5.sh -21 > $tmp-result
 echo "Error!" > $tmp-ans
 diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-3"
+rm -f $tmp-*
 
 echo "teat2-4: 異常系"
 ./report5.sh 21 -1 > $tmp-result
 echo "Error!k" > $tmp-ans
 diff $tmp-result $tmp-ans || ERROR_EXIT "error in test2-4"
+rm -f $tmp-*
